486f9185f9cf22494234260ad94461ef06e29f77
Kev Wilson <kevin.wilson@bbc.co.uk>
2019-04-18 13:41:20 +0100

    Make Mongo container accessible

--- api/src/db/connect.ts
+++ api/src/db/connect.ts
@@ -1,10 +1,11 @@
 import * as mongoose  from 'mongoose'
 
-const mongo_uri = 'mongodb://localhost:8081/admin'
+const mongo_uri = 'mongodb://localhost:27017/users'
 
 async function connect () {
   try {
     await mongoose.connect(`${mongo_uri}`, { useNewUrlParser: true })
+    console.log('connected')
   } catch (error) {
     console.log('boom!', error)
   }
