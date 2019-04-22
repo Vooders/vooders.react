de4bc6ef4c30bdc596f45fd94db163e207f2b92a
Kev Wilson <kevin.wilson@bbc.co.uk>
2019-04-22 16:32:58 +0100

    [api] Save roster

--- api/src/lib/rosters/RoszConverter.ts
+++ api/src/lib/rosters/RoszConverter.ts
@@ -1,12 +1,11 @@
 import * as decompress from 'decompress'
 import { Xml2Json } from './Xml2Json'
 import { JsonTransformer } from './JsonTransformer'
-// import { JsonTransformer } from './JsonTransformer'
-// import * as assert from 'assert'
-// import { MongoClient } from 'mongodb'
+import * as assert from 'assert'
+import { MongoClient } from 'mongodb'
 
-// const dbUrl = 'mongodb://localhost'
-// const dbName = 'api'
+const dbUrl = 'mongodb://root:example@localhost'
+const dbName = 'api'
 
 
 const dir = '/Users/wilsok23/development/personal/vooders.react/api/rosters/'
@@ -18,19 +17,20 @@ async function convertAndSave () {
     const file = await decompress(`${dir}${filename}`)
     const json = await Xml2Json.parseBuffer(file[0].data)
     const transformedJson = JsonTransformer.transform(json)
-    console.log(JSON.stringify(transformedJson, null, 2))
+    insert(transformedJson)
   } catch(error) {
     console.log(error)
   }
 }
 
-// const insert = (document: object) => {
-//   MongoClient.connect(dbUrl, { useNewUrlParser: true }, async (err, client) => {
-//     assert.equal(null, err)
-//     const db = client.db(dbName)
-//     const collection = db.collection('rosters')
-//     await collection.insertOne(document)
-//   })
-// }
+const insert = async (document: object) => {
+  MongoClient.connect(dbUrl, { useNewUrlParser: true }, async (err, client) => {
+    assert.equal(null, err)
+    const db = client.db(dbName)
+    const collection = db.collection('rosters')
+    await collection.insertOne(document)
+    client.close()
+  })
+}
 
 convertAndSave()
